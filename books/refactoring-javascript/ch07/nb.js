50293fc7c843955689e331b681170d0e95b0a9bc
letsget23 <letsget23@gmail.com>
2019-08-18 14:25:51 +0900

    remove dead code

--- books/refactoring-javascript/ch07/nb.js
+++ books/refactoring-javascript/ch07/nb.js
@@ -27,7 +27,6 @@ toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
 bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
 
 var songs = [];
-var labels = [];
 var allChords = new Set();
 var labelCounts = {};
 var labelProbabilities = {};
@@ -36,7 +35,6 @@ var probabilityOfChordsInLabels = {};
 
 function train(chords, label) {
   songs.push([label, chords]);
-  labels.push(label);
   chords.forEach(chord => allChords.add(chord));
   if (Object.keys(labelCounts).includes(label)) {
     labelCounts[label] = labelCounts[label] + 1;
